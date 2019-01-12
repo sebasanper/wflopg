5e071d573b2be16ba89eb17c7e407629577350f4
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2019-01-12 22:06:51 +0100

    various tweaks; add downwind vectors and initial context

--- code/wflopg/__init__.py
+++ code/wflopg/__init__.py
@@ -14,12 +14,12 @@ class Owflop():
 
     """
     def __init__(self):
-        coords = [
-            ('xy_coord', ['x', 'y']),  # x ~ S→N, y ~ W→E
-            ('dc_coord', ['d', 'c'])   # downwind/crosswind
-        ]
+        coords = {
+            'xy_coord': ['x', 'y'],  # x ~ S→N, y ~ W→E
+            'dc_coord': ['d', 'c']   # downwind/crosswind
+        }
         # _ds is the main working Dataset
-        self._ds = xr.Dataset(coords=dict(coords))
+        self._ds = xr.Dataset(coords=coords)
         self._ds.xy_coord.attrs['description'] = (
             "Cartesian coordinates, where ‘x’ corresponds to the "
             "South-North direction and ‘y’ to the West-East direction.")
@@ -27,20 +27,8 @@ class Owflop():
             "Cartesian coordinates determined by a given wind direction, "
             "where ‘d’ corresponds to the downwind direction "
             "and ‘c’ to the crosswind direction.")
-        # _history is the Dataset containing a history of layouts
-        self._history = xr.Dataset(coords=dict(coords[:1]))
-        self._history.xy_coord.attrs['description'] = (
-                                        self._ds.xy_coord.attrs['description'])
-        # A single turbine at the origin as default initial layout
-        initial_layout = [[0, 0]]
-        self._ds['layout'] = xr.DataArray(initial_layout,
-                                          dims=['target', 'xy_coord'])
-        self._history['initial'] = xr.DataArray(
-            initial_layout,
-            dims=['target_initial', 'xy_coord'],
-            attrs={'description': 'initial layout'}
-        )  # TODO: perhaps handle target_* dims more concisely using missing
-           #       values
+        # history of layouts as a dict with identifier, layout pairs
+        self.layouts = {}
 
     def load_problem(self, filename):
         """Load wind farm layout optimization problem file
@@ -55,68 +43,81 @@ class Owflop():
         # extract required parameters directly contained in problem document
         self.problem_uuid = problem['uuid']
         self.turbines = problem['turbines']
-        # TODO: objective?
+        # TODO: the following four parameters should be used to create the
+        #       functions that calculate the powers and deficits
+        self.objective = problem['objective']
         self.wake_model = problem['wake_model']
         self.wake_combination = problem['wake_combination']
         self.partial_wake = problem['partial_wake']
+        # TODO: the following parameter should be used to, together with the
+        #       rotor diameter, to generate the functions that check and
+        #       correct the distance constraint
         self.turbine_distance = problem.get('turbine_distance', 0)
 
         # extract and process information and data from linked documents
         with open(problem['turbine']) as f:
-            cut_in, cut_out = self.process_turbine(yaml(typ='safe').load(f))
+            self.process_turbine(yaml(typ='safe').load(f))
         with open(problem['site']) as f:
-            roughness_length = self.process_site(yaml(typ='safe').load(f))
+            self.process_site(
+                yaml(typ='safe').load(f),
+                self.rotor_radius
+            )
         with open(problem['wind_resource']) as f:
             self.process_wind_resource(
                 yaml(typ='safe').load(f),
-                roughness_length,
+                self.roughness_length,
                 problem.get('wind_direction_subdivisions', None),
                 problem.get('wind_speeds', None),
-                cut_in, cut_out
+                self.cut_in, self.cut_out
             )
         if 'layout' in problem:
             with open(problem['layout']) as f:
-                self.process_layout(yaml(typ='safe').load(f)['layout'])
+                initial_layout = yaml(typ='safe').load(f)['layout']
+        else:
+            initial_layout = [[0, 0]]
+        self.process_layout(initial_layout)
 
     def process_turbine(self, turbine):
         self.rotor_radius = turbine['rotor_radius']
         self.hub_height = turbine['hub_height']
-        rated_power = turbine['rated_power']
-        rated_speed = turbine['rated_wind_speed']
-        cut_in = turbine.get('cut_in', 0.0)
-        cut_out = turbine.get('cut_out', np.inf)
+        self.rated_power = turbine['rated_power']
+        self.rated_speed = turbine['rated_wind_speed']
+        self.cut_in = turbine.get('cut_in', 0.0)
+        self.cut_out = turbine.get('cut_out', np.inf)
         # define power curve
         if 'power_curve' in turbine:
             pc = np.array(turbine['power_curve'])
             self.power_curve = create_turbine.interpolated_power_curve(
-                                 rated_power, rated_speed, cut_in, cut_out, pc)
+                self.rated_power, self.rated_speed, self.cut_in, self.cut_out,
+                pc
+            )
         else:
             self.power_curve = create_turbine.cubic_power_curve(
-                                     rated_power, rated_speed, cut_in, cut_out)
+                 self.rated_power, self.rated_speed, self.cut_in, self.cut_out)
         # define thrust curve
         if 'thrust_coefficient' in turbine:
             self.thrust_curve = create_turbine.constant_thrust_curve(
-                                cut_in, cut_out, turbine['thrust_coefficient'])
+                                self.cut_in, self.cut_out, turbine['thrust_coefficient'])
         elif 'thrust_curve' in turbine:
             tc = np.array(turbine['thrust_curve'])
             self.thrust_curve = create_turbine.interpolated_thrust_curve(
-                                                           cut_in, cut_out, tc)
+                                                 self.cut_in, self.cut_out, tc)
         else:
             raise ValueError("Turbine document should contain either "
                              "a 'thrust_curve' or "
                              "a constant 'thrust_coefficient'")
 
-        return cut_in, cut_out
-
-    def process_site(self, site):
+    def process_site(self, site, rotor_radius):
         self.rotor_constraints = site['rotor_constraints']
         self.site_radius = site['radius']
-        # TODO: import site parcels and boundaries
-        return site.get('roughness', None)
+        # TODO: import site parcels and boundaries and together with the rotor
+        #       radius create functions to visualize and check the boundary
+        #       constraints
+        self.roughness_length = site.get('roughness', None)
 
     def process_wind_resource(self, wind_resource, roughness_length,
                               dir_subs, speeds, cut_in, cut_out):
-        reference_height = wind_resource['reference_height']
+        self.reference_height = wind_resource['reference_height']
         self.air_density = wind_resource.get('air_density', 1.2041)
         self.atmospheric_stability = wind_resource.get(
             'atmospheric_stability', None)
@@ -125,7 +126,7 @@ class Owflop():
 
         # Create the wind shear function
         self.wind_shear = create_wind.logarithmic_wind_shear(
-                                            reference_height, roughness_length)
+                                       self.reference_height, roughness_length)
 
         wind_rose = wind_resource['wind_rose']
 
@@ -183,7 +184,7 @@ class Owflop():
             speed_weights = np.array(wind_rose['speed_cpmf'])
             wc = (speeds >= cut_in) & (speeds <= cut_out)  # within cut
             speeds = speeds[wc]
-            speed_weights = speed_weights[:, wc]  # TODO: check if needs to be transposed
+            speed_weights = speed_weights[:, wc]
         else:
             raise ValueError(
                 "A conditional wind speed probability distribution "
@@ -230,17 +231,30 @@ class Owflop():
         self._ds['wind_speed_cpmf'] = (speed_weights /
                                        speed_weights.sum(dim='wind_speed'))
 
-    def process_layout(self, initial_layout):
-        self._ds.update(
-            {'layout': (['target', 'xy_coord'], np.array(initial_layout))},
-            inplace=True
-        )
-        self._history.update(
-            {'initial': (['target_initial', 'xy_coord'],
-                         np.array(initial_layout))},
-            inplace=True
+        # Store downwind unit vectors
+        # Convert inflow wind direction
+        # - from windrose (N=0, CW) to standard (E=0, CCW): 90 - wind_dir
+        # - from upwind to downwind: +180
+        # - from degrees to radians
+        directions_rad = np.radians(90 - self._ds.coords['direction'] + 180)
+        self._ds['downwind'] = xr.DataArray(
+            np.array([np.cos(directions_rad), np.sin(directions_rad)]).T,
+            dims=['direction', 'xy_coord']
         )
 
+    def process_layout(self, initial_layout):
+        # turbines affected by the wake
+        self._ds['layout'] = xr.DataArray(initial_layout,
+                                          dims=['target', 'xy_coord'])
+        self.layouts['initial'] = xr.DataArray(
+            initial_layout,
+            dims=['target', 'xy_coord'],
+            coords={'xy_coord': self._ds.coords['xy_coord']}
+        )
+        # turbines causing the wakes
+        # NOTE: currently, these are the same as the ones affected
+        self._ds['context'] = xr.DataArray(initial_layout,
+                                           dims=['source', 'xy_coord'])
 
 # dimensions = {'source', 'target',  # source and target turbine dummy dimensions
 #               'xy_coord', 'dc_coord',
