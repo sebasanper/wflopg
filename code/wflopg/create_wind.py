b812bfbdce7b3f7742071c78e5982302bb56487a
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2019-01-15 13:58:07 +0100

    correct log wind profile expression

--- code/wflopg/create_wind.py
+++ code/wflopg/create_wind.py
@@ -6,7 +6,8 @@ def logarithmic_wind_shear(reference_height, roughness_length):
     def wind_shear(heights, speeds):
         """Return sheared wind speed at the given heights
 
-        heights and speeds are assumed to be numpy arrays of the same size.
+        heights and speeds are assumed to be numpy arrays of compatible size.
 
         """
-        return speeds * np.log((heights - reference_height) / roughness_length)
+        return (speeds * np.log(heights / roughness_length)
+                       / np.log(reference_height / roughness_length))
