d7aebbafd6e6b06c3223518f688d4968ca6e811a
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2019-02-02 17:48:14 +0100

    simplify proximity repulsion function

--- code/wflopg/create_constraint.py
+++ code/wflopg/create_constraint.py
@@ -25,26 +25,19 @@ def distance(turbine_distance):
 
     """
     def proximity_repulsion(vector, distance):
-      """Return steps that can fix turbine constraints
+        """Return steps that can fix turbine constraints
 
-      Both vector and distance must be DataArrays with source and
-      target as dimensions, of adimensional xy-coordinate pairs and distances,
-      respectively.
+        Both vector and distance must be DataArrays with source and
+        target as dimensions, of adimensional xy-coordinate pairs and
+        distances, respectively.
 
-      """
-      violation = (0 < distance) & (distance < turbine_distance)
-          # NOTE: 0 excluded for distance-to-self,
-          #       so try to avoid turbines at the same location
-      if np.any(violation):
-          repulsion_step = xr.where(
-              violation,
-              (turbine_distance - distance) / 2 * vector,
-                  # just enough to fix the issue
-              [0, 0]
-          )
-          return repulsion_step.sum(dim='source')
-      else:
-          return None
+        """
+        violation = (0 < distance) & (distance < turbine_distance)
+            # NOTE: 0 excluded for distance-to-self,
+            #       so try to avoid turbines at the same location
+        return = xr.where( # just enough to fix the issue
+            violation, (turbine_distance - distance) / 2 * vector, [0, 0]
+        ).sum(dim='source')
 
     return proximity_repulsion
 
