29921c1867fd24266ab24ae1edd523fa8ea18a7a
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2019-03-05 09:11:42 +0100

    add multi-adaptive optimizer (does not work yet, because of too many extra dimensions)

--- code/wflopg/optimizers.py
+++ code/wflopg/optimizers.py
@@ -203,3 +203,96 @@ def pure_cross(owflop, max_iterations=np.inf, scaling=False):
             owflop.calculate_push_cross_vector, owflop, max_iterations)
     else:
         _iterate(owflop.calculate_push_cross_vector, owflop, max_iterations)
+
+
+def multi_adaptive(owflop, max_iterations=np.inf):
+    scale_coord = ('scale', ['-', '+'])
+    method_coord = ('method', ['down', 'back', 'cross'])
+    iterations = 0
+    corrections = ''
+    owflop._ds['layout'] = (
+        owflop._ds['layout'] * xr.DataArray(
+            [[1, 1], [1, 1], [1, 1]], coords=[method_coord, scale_coord])
+    )
+    owflop._ds['context'] = owflop._ds['layout'].rename(target='source')
+    owflop.calculate_geometry()
+    scaler = xr.DataArray([2/3, 6/5], coords=[scale_coord])
+    scaling = xr.DataArray([1, 1], coords=[scale_coord])
+    while iterations < max_iterations:
+        print('(', iterations, sep='', end=':')
+        owflop.calculate_deficit()
+        owflop.calculate_power()
+        objectives = owflop.objective()
+        i = objectives.argmin(dim='scale')
+        j = objectives.min(dim='scale').argmin('method').values.item()
+        owflop.history.append(xr.Dataset())
+        owflop.history[-1]['layout'] = (
+            owflop._ds['layout'].isel(scale=i, drop=True)
+                                .isel(method=j, drop=True))
+        owflop.history[-1]['objective'] = (
+            objectives.isel(scale=i, drop=True).isel(method=j, drop=True))
+        owflop.history[-1].attrs['corrections'] = corrections
+        owflop.history[-1].attrs['method'] = method_coord[1][j]
+        owflop.history[-1].attrs['scale'] = scaling[i[j]].values.item()
+        if len(owflop.history) == 1:
+            best = last = start = owflop.history[0]['objective']
+        else:
+            last = owflop.history[-1]['objective']
+            if last < best:
+                best = last
+            distance_from_previous = np.sqrt(
+                np.square(
+                    owflop.history[-1]['layout'] - owflop.history[-2]['layout']
+                ).sum(dim='xy')
+            )
+            # stop iterating if the largest step is smaller than 1 m
+            if distance_from_previous.max() * owflop.site_radius < 1:
+                break
+        # calculate new layout
+        scaling = scaling.isel(scale=i, drop=True) * scaler
+        # first calculate relative_wake_loss_vector just once
+        owflop._ds['relative_deficit'] = (
+            owflop._ds['relative_deficit'].isel(scale=i, drop=True)
+                                          .isel(method=j, drop=True))
+        owflop._ds['wake_loss_factor'] = (
+            owflop._ds['wake_loss_factor'].isel(scale=i, drop=True)
+                                          .isel(method=j, drop=True))
+        owflop._ds['unit_vector'] = (
+            owflop._ds['unit_vector'].isel(scale=i, drop=True)
+                                     .isel(method=j, drop=True))
+        owflop.calculate_relative_wake_loss_vector()
+        down_step = owflop.calculate_push_down_vector()
+        back_step = owflop.calculate_push_back_vector()
+        cross_step = owflop.calculate_push_cross_vector()
+        step = xr.concat([down_step, back_step, cross_step], 'method')
+        step -= step.mean(dim='target')  # remove any global shift
+        step = step * scaling  # generate the different step variants
+        _take_step(owflop, step)
+        # deal with any constraint violations in layout
+        corrections = ''
+        maybe_violations = True
+        while maybe_violations:
+            outside = ~owflop.inside(owflop._ds['layout'])['in_site']
+            any_outside = outside.any()
+            if any_outside:
+                print('s', outside.values.sum(), sep='', end='')
+                _take_step(owflop, owflop.to_border(owflop._ds['layout']))
+                corrections += 's'
+            proximity_violated = (
+                owflop.proximity_violation(owflop._ds['distance']))
+            too_close = proximity_violated.any()
+            if too_close:
+                print('p', proximity_violated.values.sum(), sep='', end='')
+                _take_step(
+                    owflop,
+                    owflop.proximity_repulsion(
+                        proximity_violated,
+                        owflop._ds['unit_vector'],
+                        owflop._ds['distance']
+                    )
+                )
+                corrections += 'p'
+            print(',', end='')
+            maybe_violations = any_outside & too_close
+        print(')', end=' ')
+        iterations += 1
