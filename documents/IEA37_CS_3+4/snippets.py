d9e9243485f8d31ffdeb565a9790642304ad5b62
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2019-09-21 14:44:09 +0200

    add snippets script file for IEA37 Case Studies 3+4

---
+++ documents/IEA37_CS_3+4/snippets.py
@@ -0,0 +1,20 @@
+import xarray as xr
+import wflopg
+
+# plots wake losses vs. wind sector subdivisions
+subs = [1, 2, 3, 4, 5, 9, 12]
+n = len(subs)
+results = xr.DataArray(np.zeros((n, n)), coords=[('subdivisions', subs), ('layout', subs)])
+
+for subdivisions in results.subdivisions.values:
+    for layout in results.layout.values:
+        o = wflopg.Owflop()
+        o.load_problem('problem-IEA37_CS3-sub{}.yaml'.format(subdivisions),
+                       layout_filename='layout-IEA37_CS3-sub{}.yaml'.format(layout))
+        o.calculate_geometry()
+        o.calculate_wakeless_power()
+        o.calculate_deficit()
+        o.calculate_power()
+        results.loc[{'subdivisions': subdivisions, 'layout': layout}] = o._ds['average_expected_wake_loss_factor']
+        
+results.plot.line(x='subdivisions')
