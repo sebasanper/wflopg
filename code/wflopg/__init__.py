6debbd810987360841b9741c51930921a13bdb65
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2020-06-18 11:05:28 +0200

    make loading and processing of input less coupled

--- code/wflopg/__init__.py
+++ code/wflopg/__init__.py
@@ -53,7 +53,11 @@ class Owflop():
 
         # extract and process information and data from linked documents
         self.load_turbine(problem['turbine'])
-        self.load_site(problem['site'], self.rotor_radius)
+        self.load_site(problem['site'])
+        self.rotor_radius_adim = self.rotor_radius / self.site_radius
+        self.rotor_diameter_adim = 2 * self.rotor_radius_adim
+        self.process_turbine()
+        self.process_site()
         dir_subs = problem.get('wind_direction_subdivisions', None)
         speeds = problem.get('wind_speeds', None)
         if wind_resource is None:
@@ -67,7 +71,9 @@ class Owflop():
                 speeds = wind_resource['speeds']
         elif isinstance(wind_resource, str):
             wind_resource_file = wind_resource
-        self.load_wind_resource(wind_resource_file, self.roughness_length)
+        self.load_wind_resource(wind_resource_file)
+        self.wind_shear = create_wind.logarithmic_wind_shear(
+            self.reference_height, self.roughness_length)
         self.process_wind_resource(
             self.hub_height, self.cut_in, self.cut_out, dir_subs, speeds)
 
@@ -133,19 +139,25 @@ class Owflop():
             order = pc[:, 0].argsort()
             self.power_curve_data = _xr.DataArray(
                 pc[order, 1], coords=[('speed', pc[order, 0])])
+        else:
+            self.power_curve_data = None
         if 'thrust_coefficient' in turbine:
             self.thrust_coefficient = turbine['thrust_coefficient']
+            self.thrust_curve = None
         elif 'thrust_curve' in turbine:
             tc = _np.array(turbine['thrust_curve'])
             order = tc[:, 0].argsort()
             self.thrust_curve_data = _xr.DataArray(
                 tc[order, 1], coords=[('speed', tc[order, 0])])
+            self.thrust_coefficient = None
         else:
             raise ValueError("Turbine document should contain either "
                              "a 'thrust_curve' or "
                              "a constant 'thrust_coefficient'")
+
+    def process_turbine(self):
         # define power curve
-        if 'power_curve' in turbine:
+        if self.power_curve_data is not None:
             self.power_curve = create_turbine.interpolated_power_curve(
                 self.rated_power, self.rated_speed, self.cut_in, self.cut_out,
                 self.power_curve_data
@@ -154,14 +166,14 @@ class Owflop():
             self.power_curve = create_turbine.cubic_power_curve(
                  self.rated_power, self.rated_speed, self.cut_in, self.cut_out)
         # define thrust curve
-        if 'thrust_curve' in turbine:
+        if self.thrust_curve is not None:
             self.thrust_curve = create_turbine.interpolated_thrust_curve(
                 self.cut_in, self.cut_out, self.thrust_curve_data)
-        else:  # constant thrust curve
+        elif self.thrust_coefficient is not None:  # constant thrust curve
             self.thrust_curve = create_turbine.constant_thrust_curve(
                 self.cut_in, self.cut_out, self.thrust_coefficient)
 
-    def load_site(self, filename, rotor_radius):
+    def load_site(self, filename):
         with open(filename) as f:
             site = _yaml_load(f)
         self.roughness_length = site.get('roughness', None)
@@ -175,18 +187,21 @@ class Owflop():
                     site['location']['adhoc'], coords=[('xy', COORDS['xy'])]
                 ) * 1e3  # km to m
         self.site_parcels = site['parcels']
+        self.site_boundaries = site.get('boundaries', None)
+        
+    def process_site(self):
         # Process parcels
-        self.parcels = create_site.parcels(
-            self.site_parcels, rotor_radius / self.site_radius)
+        self.parcels = create_site.parcels(self.site_parcels,
+                                           self.rotor_radius_adim)
         # create function that reports whether a turbine is inside the site
         self.inside = create_constraint.inside_site(self.parcels)
         # create function to generate site constraint violation fixup steps
         self.to_border = create_constraint.site(self.parcels)
         # Process boundaries
-        if 'boundaries' in site:
-            self.boundaries = create_site.boundaries(site['boundaries'])
+        if self.site_boundaries is not None:
+            self.boundaries = create_site.boundaries(self.site_boundaries)
 
-    def load_wind_resource(self, filename, roughness_length):
+    def load_wind_resource(self, filename):
         with open(filename) as f:
             wind_resource = _yaml_load(f)
         self.reference_height = wind_resource['reference_height']
@@ -224,10 +239,6 @@ class Owflop():
                 "A conditional wind speed probability distribution "
                 "should be given either as parameters for conditional Weibull "
                 "distributions or as a conditional probability mass function.")
-
-        # Create the wind shear function
-        self.wind_shear = create_wind.logarithmic_wind_shear(
-                                       self.reference_height, roughness_length)
 
     def process_wind_resource(self, hub_height, cut_in, cut_out,
                               dir_subs=None, speeds=None):
