351776e974eac9ea6409c59e09f94d96cbdffb0c
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2020-06-18 23:56:15 +0200

    move adaptive scaling into the plain optimization interation code

--- code/wflopg/optimizers.py
+++ code/wflopg/optimizers.py
@@ -9,7 +9,8 @@ from wflopg.create_layout import _take_step, fix_constraints
 from wflopg.helpers import rss
 
 
-def _iterate(step_generator, owflop, max_iterations, multiplier,
+def _iterate(step_generator, owflop, max_iterations,
+             multiplier, scaling=True,
              visualize=False):
 
     def layout2power(owflop):
@@ -17,10 +18,11 @@ def _iterate(step_generator, owflop, max_iterations, multiplier,
         owflop.calculate_deficit()
         owflop.calculate_power()
 
-    def update_history(owflop, corrections='', bound=_np.nan):
+    def update_history(owflop, layout, objective, bound=_np.nan,
+                       corrections='', multiplier=multiplier):
         owflop.history.append(_xr.Dataset())
-        owflop.history[-1]['layout'] = owflop._ds.layout
-        owflop.history[-1]['objective'] = owflop.objective()
+        owflop.history[-1]['layout'] = layout
+        owflop.history[-1]['objective'] = objective
         owflop.history[-1]['objective_bound'] = bound
         owflop.history[-1].attrs['corrections'] = corrections
         owflop.history[-1].attrs['scale'] = multiplier
@@ -58,14 +60,26 @@ def _iterate(step_generator, owflop, max_iterations, multiplier,
         vis.draw_boundaries(axes['layout'], owflop)
         _plt.pause(0.1)
 
-    layout2power(owflop)
-    update_history(owflop)
+    if scaling is True:
+        scaling = [.8, 1.1]
+    elif scaling is False:
+        scaling = [1]
+    scaler = _xr.DataArray(_np.float64(scaling), dims=('scale',))
+    
+    owflop.calculate_geometry()
+    owflop.calculate_deficit()
+    owflop.calculate_power()
+    update_history(owflop, owflop._ds.layout, owflop.objective())
     best = start = owflop.history[0].objective
     if visualize:
         axes = setup_visualization(owflop)
     for iteration in range(1, max_iterations+1):
         print(iteration, end=': ')
-        # calculate new layout
+        owflop._ds['layout'] = owflop.history[-1].layout
+        owflop._ds['context'] = owflop._ds.layout.rename(target='source')
+        owflop.calculate_geometry()
+        owflop.calculate_deficit()
+        # calculate step
         owflop.calculate_relative_wake_loss_vector()
         step = step_generator()
         # normalize the step to the largest pseudo-gradient
@@ -73,16 +87,21 @@ def _iterate(step_generator, owflop, max_iterations, multiplier,
         step /= distance.max('target')
         # remove any global shift # TODO: remove shift before normalization
         step -= step.mean(dim='target')
-        step *= multiplier * owflop.rotor_diameter_adim
         # take step
+        multiplier = scaler * multiplier
+        step = step * multiplier * owflop.rotor_diameter_adim
         _take_step(owflop, step)
         # fix any constraint violation
         corrections = fix_constraints(owflop)
         # evaluate new layout
-        layout2power(owflop)
+        owflop.calculate_deficit()
+        owflop.calculate_power()
+        i = owflop.objective().argmin('scale')
+        layout = owflop._ds.layout.isel(scale=i, drop=True)
+        current = owflop.objective().isel(scale=i, drop=True)
         bound = best + (start - best) / iteration
-        update_history(owflop, corrections, bound)
-        current = owflop.history[-1].objective
+        multiplier = multiplier.isel(scale=i, drop=True)
+        update_history(owflop, layout, current, bound, corrections, multiplier)
         if visualize:
             iterate_visualization(axes, owflop)
         # check best layout and criteria for early termination
@@ -97,147 +116,37 @@ def _iterate(step_generator, owflop, max_iterations, multiplier,
             if distance_from_previous.max() < .1:
                 break
 
-
-def _adaptive_iterate(step_generator, owflop, max_iterations, multiplier,
-                      scaler=[.5, 1.1], visualize=False):
-    if visualize:
-        fig = _plt.figure()
-        grid = _gs.GridSpec(3, 5)
-        ax_windrose = fig.add_subplot(grid[0, :2], polar=True)
-        vis.draw_windrose(ax_windrose, owflop._ds.direction_pmf)
-        ax_convergence = fig.add_subplot(grid[1, :2])
-        ax_scaling = fig.add_subplot(grid[2, :2], sharex=ax_convergence)
-        ax_layout = fig.add_subplot(grid[:, 2:])
-        vis.site_setup(ax_layout)
-        vis.draw_turbines(ax_layout, owflop, owflop._ds.layout,
-                          proximity=True, in_or_out=True)
-        vis.draw_boundaries(ax_layout, owflop)
-        grid.tight_layout(fig)
-        _plt.pause(.10)
-    scale_coord = ('scale', ['-', '+'])
-    iterations = 0
-    best = last = start = 0
-    bound = _np.nan
-    corrections = ''
-    owflop._ds['layout'] = (
-        owflop._ds.layout * _xr.DataArray([1, 1], coords=[scale_coord])
-    )
-    owflop._ds['context'] = owflop._ds.layout.rename(target='source')
-    owflop.calculate_geometry()
-    scaler = _xr.DataArray(scaler, coords=[scale_coord])
-    scaling = _xr.DataArray([1, 1], coords=[scale_coord])
-    while iterations < max_iterations:
-        # stop iterating if no real objective improvement is being made
-        if iterations > 0:
-            bound = best + (start - best) / iterations
-            if last > bound:
-                break
-        print(iterations, end=': ')
-        owflop.calculate_deficit()
-        owflop.calculate_power()
-        objectives = owflop.objective()
-        i = objectives.argmin()
-        # we continue from best layout
-        owflop._ds['layout'] = (
-            owflop._ds.layout.isel(scale=i, drop=True)
-            * _xr.DataArray([1, 1], coords=[scale_coord])
-        )
-        owflop._ds['context'] = owflop._ds.layout.rename(target='source')
-        # update history
-        owflop.history.append(_xr.Dataset())
-        owflop.history[-1]['layout'] = (
-            owflop._ds.layout.isel(scale=i, drop=True))
-        owflop.history[-1]['objective'] = objectives.isel(scale=i, drop=True)
-        owflop.history[-1]['objective_bound'] = bound
-        owflop.history[-1].attrs['corrections'] = corrections
-        owflop.history[-1].attrs['scale'] = scaling[i].item()
-        if visualize:
-            ax_convergence.clear()
-            vis.draw_convergence(ax_convergence, owflop.history)
-            ax_scaling.clear()
-            vis.draw_scaling(ax_scaling, owflop.history)
-            ax_layout.clear()
-            vis.site_setup(ax_layout)
-            vis.connect_layouts(ax_layout,
-                                [ds.layout for ds in owflop.history])
-            vis.draw_turbines(ax_layout, owflop, owflop.history[0].layout)
-            vis.draw_turbines(ax_layout, owflop, owflop.history[-1].layout,
-                              proximity=True, in_or_out=True)
-            vis.draw_boundaries(ax_layout, owflop)
-            _plt.pause(0.1)
-        if len(owflop.history) == 1:
-            best = last = start = owflop.history[0].objective
-        else:
-            last = owflop.history[-1].objective
-            if last < best:
-                best = last
-            distance_from_previous = rss(
-                owflop.history[-1].layout - owflop.history[-2].layout,
-                dim='xy'
-            )
-            # stop iterating if the largest step is smaller than D/10
-            if distance_from_previous.max() < owflop.rotor_diameter_adim / 10:
-                break
-        # calculate new layout
-        scaling = scaling.isel(scale=i) * scaler
-        # first calculate relative_wake_loss_vector just once
-        owflop._ds['relative_deficit'] = (
-            owflop._ds.relative_deficit.isel(scale=i))
-        owflop._ds['wake_loss_factor'] = (
-            owflop._ds.wake_loss_factor.isel(scale=i))
-        owflop._ds['unit_vector'] = owflop._ds.unit_vector.isel(scale=i)
-        owflop.calculate_relative_wake_loss_vector()
-        step = step_generator()
-        # normalize the step to the largest pseudo-gradient
-        distance = rss(step, dim='xy')
-        step /= distance.max('target')
-        # remove any global shift
-        step -= step.mean(dim='target')
-        step *= multiplier * owflop.rotor_diameter_adim
-        step = step * scaling  # generate the different step variants
-        _take_step(owflop, step)
-        corrections = fix_constraints(owflop)
-        iterations += 1
-
-
-def pure_down(owflop, max_iterations=_sys.maxsize,
-              scaling=False, scaler=[.5, 1.1], multiplier=3, visualize=False):
+def pure_down(owflop,
+               max_iterations=_sys.maxsize, scaling=[.8, 1.1], multiplier=1,
+               visualize=False):
     """Optimize the layout using push-down only
 
     The problem object owflop is assumed to have a problem loaded, but not
     necessarily have any further actions applied.
 
     """
-    if scaling:
-        _adaptive_iterate(owflop.calculate_push_down_vector, owflop,
-                          max_iterations, multiplier, scaler=scaler,
-                          visualize=visualize)
-    else:
-        _iterate(owflop.calculate_push_down_vector, owflop,
-                 max_iterations, multiplier,
-                 visualize=visualize)
+    _iterate(owflop.calculate_push_down_vector, owflop,
+             max_iterations, multiplier, scaling,
+             visualize)
 
 
-def pure_back(owflop, max_iterations=_sys.maxsize,
-              scaling=False, scaler=[.5, 1.1], multiplier=3, visualize=False):
+def pure_back(owflop,
+               max_iterations=_sys.maxsize, scaling=[.8, 1.1], multiplier=1,
+               visualize=False):
     """Optimize the layout using push-back only
 
     The problem object owflop is assumed to have a problem loaded, but not
     necessarily have any further actions applied.
 
     """
-    if scaling:
-        _adaptive_iterate(owflop.calculate_push_back_vector, owflop,
-                          max_iterations, multiplier, scaler=scaler,
-                          visualize=visualize)
-    else:
-        _iterate(owflop.calculate_push_back_vector, owflop,
-                 max_iterations, multiplier,
-                 visualize=visualize)
+    _iterate(owflop.calculate_push_back_vector, owflop,
+             max_iterations, multiplier, scaling,
+             visualize)
 
 
-def mixed_down_and_back(owflop, max_iterations=_sys.maxsize,
-                        scaling=False, scaler=[.5, 1.1], multiplier=3,
+def mixed_down_and_back(owflop,
+                        max_iterations=_sys.maxsize,
+                        scaling=[.8, 1.1], multiplier=1,
                         visualize=False):
     """Optimize the layout using a mixture of push-down and push-back
 
@@ -248,32 +157,23 @@ def mixed_down_and_back(owflop, max_iterations=_sys.maxsize,
     def step_generator():
         return (owflop.calculate_push_down_vector()
                 + owflop.calculate_push_back_vector()) / 2
-
-    if scaling:
-        _adaptive_iterate(step_generator, owflop, max_iterations,
-                          multiplier, scaler=scaler,
-                          visualize=visualize)
-    else:
-        _iterate(step_generator, owflop, max_iterations, multiplier,
-                 visualize=visualize)
+    
+    _iterate(step_generator, owflop, max_iterations, multiplier, scaling,
+             visualize)
 
 
-def pure_cross(owflop, max_iterations=_sys.maxsize, scaling=False,
-               scaler=[.5, 1.1], multiplier=3, visualize=False):
-    """Optimize the layout using push-back only
+def pure_cross(owflop,
+               max_iterations=_sys.maxsize, scaling=[.8, 1.1], multiplier=1,
+               visualize=False):
+    """Optimize the layout using push-cross only
 
     The problem object owflop is assumed to have a problem loaded, but not
     necessarily have any further actions applied.
 
     """
-    if scaling:
-        _adaptive_iterate(owflop.calculate_push_cross_vector, owflop,
-                          max_iterations, multiplier, scaler=scaler,
-                          visualize=visualize)
-    else:
-        _iterate(owflop.calculate_push_cross_vector, owflop,
-                 max_iterations, multiplier,
-                 visualize=visualize)
+    _iterate(owflop.calculate_push_cross_vector, owflop,
+             max_iterations, multiplier, scaling,
+             visualize)
 
 
 def multi_adaptive(owflop, max_iterations=_sys.maxsize,
