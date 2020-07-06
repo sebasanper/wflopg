b8837cdc2b31f39fed78d041bba411b9d5680377
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2020-07-06 23:27:50 +0200

    Layout class: make state hidden

--- code/wflopg/create_layout.py
+++ code/wflopg/create_layout.py
@@ -1,6 +1,5 @@
 import numpy as _np
 import xarray as _xr
-import copy as _cp
 
 from wflopg.constants import COORDS
 import wflopg.helpers as _hs
@@ -13,10 +12,10 @@ class Layout():
         
     Parameters
     ----------
-    filename
+    filename : str
         The path to a file containing a wind farm layout
         description satisfying the wind farm layout schema.
-    kwargs
+    kwargs : dict
         A (nested) `dict` containing a (partial) description
         of a wind farm layout according to the wind farm layout
         schema. It can be used both to override elements of
@@ -25,23 +24,43 @@ class Layout():
         
     Attributes
     ----------
-    abspos
-        ***
-    relpos
-        ***
+    STATE_TYPES : frozenset
+        The set of state types stored for layouts.
+        Contains:
+
+        * `'target'`: wake targets, i.e., turbines that contribute to
+          power production (usually all the farm's turbines)
+        * `'source'`: wake sources, i.e., turbines that generate wakes
+          (usually farm turbines and all the surrounding turbines)
+        * `'movable'`: turbines that are allowed to be moved
+          (usually all the farm turbines, although some may be kept fixed
+          at some point in the design)
+        * `'context'`: turbines that belong to the surroundings
+          and not to the farm that is being optimized
+          (these should not be considered for constraint checks)
 
     Methods
     -------
-    initialize_relative_positions(frm, to)
-        Add and initialize relative positions to wind farm layout object.
-    get_distances()
-        Get and, as needed, calculate distances between locations.
+    get_positions()
+        Get positions.
+    initialize_relative_positions(pos_from, pos_to)
+        Add and initialize relative positions data in object.
+    get_relative_positions()
+        Get and, as needed, calculate relative positions.
     get_angles()
         Get and, as needed, calculate angles between locations.
-    shift(step):
-        Shift the layout by a given step.
+    get_distances()
+        Get and, as needed, calculate distances between locations.
+    get_normed_relative_positions():
+        Get and, as needed, calculate normed relative positions.
+    set_positions(positions):
+        Set the positions of the layout.
+    shift_positions(step):
+        Shift the positions of the layout by a given step.
         
     """
+    STATE_TYPES = frozenset({'target', 'source', 'movable', 'context'})
+    
     def __init__(self, filename=None, **kwargs):
         """Create a wind farm layout object."""
         # TODO: add reference to actual wind farm layout schema
@@ -52,18 +71,119 @@ class Layout():
         layout_dict.update(kwargs)
         # TODO: check layout_dict with schema using jsonschema
         
-        layout_array = _np.array(layout_dict['layout'])
-        self.abspos = _xr.Dataset(coords={'pos': range(len(layout_array))})
-        for name, index in {('x', 0), ('y', 1)}:
-            self.abspos[name] = ('pos', layout_array[:, index])
-        for name, val in {'target': True, 'source': True,
-                          'movable': True, 'context': False}.items():
-            self.abspos[name] = _xr.full_like(self.abspos.pos, val, dtype=bool)
+        layout = layout_dict['layout']
+        if isinstance(layout, _xr.core.dataset.Dataset):
+            ds = layout
+        else:
+            if isinstance(layout, list):
+                layout_array = _np.array(layout_dict['layout'])
+            elif isinstance(layout, _np.ndarray):
+                layout_array = layout
+            else:
+                raise ValueError(f"Incorrect type of layout: {layout}.")
+            ds = _xr.Dataset(coords={'pos': range(len(layout_array))})
+            for name, index in {('x', 0), ('y', 1)}:
+                ds[name] = ('pos', layout_array[:, index])
+        self._state = ds
             
         # TODO: add other layout_dict properties as attributes to self?
 
+    def get_positions(self):
+        """Get positions.
+        
+        Returns
+        -------
+        `xarray.DataSet`
+            ***
+        
+        """
+        return self._state[['x', 'y']]
+    
+
+    def set_positions(self, positions):
+        """Set the positions of the layout.
+        
+        Parameters
+        ----------
+        positions
+            An `xarray.Dataset` with `x` and `y` `xarray.DataArray`s
+            with a `pos` coordinate array containing a subset of
+            positions present in the object's own `layout` attribute.
+        
+        """
+        try:
+            del self._rel  # rel becomes outdated when layout is changed
+        except AttributeError:
+            pass
+        if not self._state.movable.sel(pos=positions.pos).all():
+            raise IndexError("You may not change non-movable turbines.")
+        for z in {'x', 'y'}:
+            self._state[z].loc[dict(pos=positions.pos)] += positions[z]
+            # TODO: do this in one go?
+
+    def shift_positions(self, step):
+        """Shift the positions of the layout by a given step.
+        
+        Parameters
+        ----------
+        step
+            An `xarray.Dataset` with `x` and `y` `xarray.DataArray`s
+            with a `pos` coordinate array containing a subset of
+            positions present in the object's own `layout` attribute.
+        
+        """
+        self.set_positions(self.positions.loc[dict(pos=step.pos)] + step)    
+    
+    def get_state(self, *args):
+        """Get state information.
+        
+        Parameters
+        ----------
+        args
+            ***
+        
+        Returns
+        -------
+        `xarray.Dataset`
+            ***
+        
+        """
+        illegal = set(args) - self.STATE_TYPES
+        if illegal:
+            raise ValueError(f"The arguments {illegal} are illegal types, "
+                             f"i.e., not in {self.STATE_TYPES}.")
+        undefined = set(args) - set(self._state.keys())
+        if undefined:
+            raise ValueError(f"The arguments {undefined} are not currently "
+                             "defined for this layout. Use the ‘set_state’ "
+                             "method for these first.")
+        return self._state[list(args)]
+    
+    def set_state(self, **kwargs):
+        """Set state information.
+        
+        Parameters
+        ----------
+        kwargs
+            ***
+        
+        """
+        args = kwargs.keys()
+        if not self.STATE_TYPES.issuperset(set(args)):
+            raise ValueError(f"The arguments given, {args}, includes illegal "
+                             f"types, i.e., not in {self.STATE_TYPES}.")
+        for state, val in kwargs.items():
+            if isinstance(val, _xr.core.dataarray.DataArray):
+                da = val
+            elif isinstance(val, bool):
+                da = _xr.full_like(self._state.pos, val, dtype=bool)
+            else:
+                raise ValueError(
+                    f"Incorrect type of description for state {state}: {val}.")
+            self._state[state] = da
+
     def initialize_relative_positions(self, pos_from, pos_to):
-        """Add and initialize relative positions to wind farm layout object.
+        """Add and initialize relative positions data in object.
         
         Parameters
         ----------
@@ -75,21 +195,50 @@ class Layout():
             to calculate relative positions to.
         
         """
-        ds = self.abspos
-        self.relpos = _xr.Dataset(
+        ds = self._state
+        self._rel = _xr.Dataset(
             coords={'pos_from': ds.pos[pos_from].rename({'pos': 'pos_from'}),
                     'pos_to': ds.pos[pos_to].rename({'pos': 'pos_to'})}
         )
-        for z in {'x', 'y'}:
-            da = ds[z]
-            self.relpos[z] = (da.sel(pos=self.relpos['pos_to'])
-                              - da.sel(pos=self.relpos['pos_from']))
-    
-    def _has_relpos_check(self):
-        """Raise AttributeError if the relpos attribute is not present."""
-        if not hasattr(self, 'relpos'):
+
+    def _has_rel_check(self):
+        """Raise AttributeError if the _rel attribute is not present."""
+        if not hasattr(self, '_rel'):
             raise AttributeError(
                 "Call the ‘initialize_relative_positions’ method first.")
+
+    def get_relative_positions(self):
+        """Get and, as needed, calculate relative positions.
+            
+        Returns
+        -------
+        `xarray.DataSet`
+            ***
+        
+        """
+        self._has_rel_check()
+        if ('x' not in self._rel) or ('y' not in self._rel):
+            for z in {'x', 'y'}:
+                da = self._state[z]
+                self._rel[z] = (da.sel(pos=self._rel['pos_to'])
+                                - da.sel(pos=self._rel['pos_from']))
+                # TODO: do this in one go using self._state[['x', 'y']]?
+        return self._rel[['x', 'y']]
+    
+    def get_angles(self):
+        """Get and, as needed, calculate angles between positions.
+            
+        Returns
+        -------
+        `xarray.DataArray`
+            ***
+        
+        """
+        self._has_rel_check()
+        if 'angle' not in self._rel:
+            relxy = self.get_relative_positions()
+            self._rel['angle'] = _np.arctan2(relxy.y, relxy.x)
+        return self._rel.angle    
 
     def get_distances(self):
         """Get and, as needed, calculate distances between positions.
@@ -100,15 +249,15 @@ class Layout():
             ***
         
         """
-        self._has_relpos_check()
-        if 'distance' not in self.relpos:
-            self.relpos['distance'] = (
-                _np.sqrt(_np.square(self.relpos.x) + _np.square(self.relpos.y))
-            )
-        return self.relpos.distance
-        
-    def get_angles(self):
-        """Get and, as needed, calculate angles between positions.
+        self._has_rel_check()
+        if 'distance' not in self._rel:
+            relxy = self.get_relative_positions()
+            self._rel['distance'] = _np.sqrt(
+                _np.square(relxy.x) + _np.square(relxy.y))
+        return self._rel.distance
+
+    def get_normed_relative_positions(self):
+        """Get and, as needed, calculate normed relative positions.
             
         Returns
         -------
@@ -116,38 +265,15 @@ class Layout():
             ***
         
         """
-        self._has_relpos_check()
-        if 'angle' not in self.relpos:
-            self.relpos['angle'] = _np.arctan2(self.relpos.y, self.relpos.x)
-        return self.relpos.angle
-        
-    def shift(self, step):
-        """Shift the layout by a given step.
-        
-        Parameters
-        ----------
-        step
-            An `xarray.Dataset` with `x` and `y` `xarray.DataArray`s
-            with a `pos` coordinate array containing a subset of
-            positions present in the object's own `layout` attribute.
-            
-        Returns
-        -------
-        `Layout`
-            The layout shifted by the given step.
-            The original layout is unaffected.
-        
-        """
-        other = _cp.deepcopy(self)
-        try:
-            del other.relpos  # rel becomes outdated when layout is changed
-        except AttributeError:
-            pass
-        if not other.abspos.movable.sel(pos=step.pos).all():
-            raise IndexError("The step may not shift non-movable turbines.")
-        for z in {'x', 'y'}:
-            other.abspos[z].loc[dict(pos=step.pos)] += step[z]
-        return other
+        self._has_rel_check()
+        if ('x_normed' not in self._rel) or ('y_normed' not in self._rel):
+            relxy = self.get_relative_positions()
+            dists = self.get_distances()
+            dists = dists.where(dists != 0, 1)  # avoid division by zero
+            for z in {'x', 'y'}:
+                self._rel[z + '_normed'] = relxy[z] / dists
+                # TODO: do this in one go with relxy / dists?
+        return self._rel[['x_normed', 'y_normed']]
 
 def hexagonal(turbines, site_parcels, site_violation_distance, to_border):
     """Create hexagonal—so densest—packing to cover site
